3579b9a01e4e9768fa9c3608bb0fef04f4059f1f
irarmdhnty <irarmdhnty03@gmail.com>
2022-09-24 23:43:25 +0700

    day 8

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ func main() {
 	r.HandleFunc("/detail/{index}", detail).Methods("GET")
 	r.HandleFunc("/delete/{index}", delete).Methods("GET")
 	r.HandleFunc("/edit/{index}", update).Methods("GET")
+	r.HandleFunc("/edit-project/{index}", editProject).Methods("POST")
 
 	fmt.Println("server on in port 8000")
 	http.ListenAndServe("localhost:8000", r)
@@ -31,7 +32,6 @@ func main() {
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/html; charset=utf-8")
-
 	tmpl, err := template.ParseFiles("views/index.html")
 
 	if err != nil {
@@ -78,8 +78,6 @@ type Project struct {
 
 var data = []Project{}
 
-// editdata
-
 func addProject(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
@@ -93,17 +91,14 @@ func addProject(w http.ResponseWriter, r *http.Request) {
 	var technologies []string
 	technologies = r.Form["technologies"]
 
-	// fmt.Println(technologies)
-	// fmt.Println(start_date)
-
 	layout := "2006-01-02"
 	dateStart, _ := time.Parse(layout, start_date)
-	dateEnd, _ := time.Parse(layout, end_date)
+	dateEnd, _ := time.Parse(layout, end_date )
 
 	hours := dateEnd.Sub(dateStart).Hours()
 	daysInHours := hours / 24
 	monthInDay := daysInHours / 30
-	yearInMonth := monthInDay / 12 // Njir prettier ya
+	yearInMonth := monthInDay / 12
 
 	var duration string
 	var month, _ float64 = math.Modf(monthInDay)
@@ -111,30 +106,20 @@ func addProject(w http.ResponseWriter, r *http.Request) {
 
 	if year > 0 {
 		duration = strconv.FormatFloat(year, 'f', 0, 64) + " Years"
-		// fmt.Println(year, " Years")
 	} else if month > 0 {
 		duration = strconv.FormatFloat(month, 'f', 0, 64) + " Months"
-		// fmt.Println(month, " Months")
 	} else if daysInHours > 0 {
 		duration = strconv.FormatFloat(daysInHours, 'f', 0, 64) + " Days"
-		// fmt.Println(daysInHours, " Days")
-	} else if hours > 0 {{{  }}
-		duration = strconv.FormatFloat(hours, 'f', 0, 64) + " Hours"
-		// fmt.Println(hours, " Hours")
 	} else {
 		duration = "0 Days"
-		// fmt.Println("0 Days")
-	}
 
-	// fmt.Println(daysInHours)
-	// fmt.Println(month)
-	// fmt.Println(year)
+	}
 
 	var newData = Project{
 		Name:         name,
 		Start_date:   start_date,
 		End_date:     end_date,
-		Duration: duration,
+		Duration:     duration,
 		Desc:         desc,
 		Technologies: technologies,
 	}
@@ -161,10 +146,12 @@ func detail(w http.ResponseWriter, r *http.Request) {
 	for i, data := range data {
 		if index == i {
 			Detail = Project{
-				Name:       data.Name,
-				Start_date: data.Start_date,
-				End_date:   data.End_date,
-				Desc:       data.Desc,
+				Name:         data.Name,
+				Start_date:   data.Start_date,
+				End_date:     data.End_date,
+				Desc:         data.Desc,
+				Duration:     data.Duration,
+				Technologies: data.Technologies,
 			}
 		}
 	}
@@ -184,22 +171,91 @@ func delete(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", http.StatusFound)
 }
 
+func editProject(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	index, _ := strconv.Atoi(mux.Vars(r)["index"])
+
+	var name = r.PostForm.Get("inputName")
+	var start_date = r.PostForm.Get("startDate")
+	var end_date = r.PostForm.Get("endDate")
+	var desc = r.PostForm.Get("desc")
+	var technologies []string
+	technologies = r.Form["technologies"]
+
+	// fmt.Println(technologies)
+	// fmt.Println(start_date)
+
+	layout := "2006-01-02"
+	dateStart, _ := time.Parse(layout, start_date)
+	dateEnd, _ := time.Parse(layout, end_date)
+
+	hours := dateEnd.Sub(dateStart).Hours()
+	daysInHours := hours / 24
+	monthInDay := daysInHours / 30
+	yearInMonth := monthInDay / 12
+
+	var duration string
+	var month, _ float64 = math.Modf(monthInDay)
+	var year, _ float64 = math.Modf(yearInMonth)
+
+	if year > 0 {
+		duration = strconv.FormatFloat(year, 'f', 0, 64) + " Years"
+	} else if month > 0 {
+		duration = strconv.FormatFloat(month, 'f', 0, 64) + " Months"
+	} else if daysInHours > 0 {
+		duration = strconv.FormatFloat(daysInHours, 'f', 0, 64) + " Days"
+	} else {
+		duration = "0 Days"
+	}
+
+	var newData = Project{
+		Name:         name,
+		Start_date:   start_date,
+		End_date:     end_date,
+		Duration:     duration,
+		Desc:         desc,
+		Technologies: technologies,
+	}
+
+	data[index] = newData
+	// fmt.Println(data)
+
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
+}
+
 func update(w http.ResponseWriter, r *http.Request) {
-	// {{ iseditmode?update : /add-project }}
 	w.Header().Set("Content-type", "text/html; charset=utf-8")
-	tmpl, err := template.ParseFiles("views/addProject.html")
+	tmpl, err := template.ParseFiles("views/editProject.html")
 	if err != nil {
 		w.Write([]byte(err.Error()))
 		return
 	}
 
+	var Edit = Project{}
 	index, _ := strconv.Atoi(mux.Vars(r)["index"])
 	fmt.Println(index)
 
-	var Edit = Project{}
-	data := map[string]interface{}{
-		"Edit": Edit,
+	for i, data := range data {
+		if index == i {
+			Edit = Project{
+				Name:         data.Name,
+				Start_date:   data.Start_date,
+				End_date:     data.End_date,
+				Desc:         data.Desc,
+				Technologies: data.Technologies,
+			}
+		}
 	}
 
+	data := map[string]interface{}{
+		"Index": index,
+		"Edit": Edit,
+	}
+	// fmt.Println(data)
 	tmpl.Execute(w, data)
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
 }
